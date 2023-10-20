b6f155681699e4040c75f8bed0912f7b6ec952a8
danersound <andre.barreto28@gmail.com>
2023-10-20 15:06:37 +0200

    added more notes abou lesson, must finish lesson 7

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return{
+            cart:0,
+            product:'Socks',
+            description: ' this socks are really comfortable',
+            image: './assets/images/socks_green.jpg',
+            url:'www.google.it',
+            inventory: 0,
+            onSale:false,
+            details:['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:2234, color:'green', size:'small', image: './assets/images/socks_green.jpg'},
+                {id:2235, color:'blue', size:'medium', image: './assets/images/socks_blue.jpg'}
+            ],
+            sizes:['extra-small', 'small', 'medium', 'large', 'extra-large', 'XXLARGE']
+        }
+    },
+    methods:{
+        addToCart(){
+           this.cart +=1 
+        },
+        removeItem(){
+            if(this.cart != 0){
+                this.cart -=1
+            }
+         },
+        updateImage(variantImage){
+            this.image = variantImage
+        }
+
+    }
+})
+
